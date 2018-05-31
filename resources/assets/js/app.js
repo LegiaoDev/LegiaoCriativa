b7ba9c3adc3f893b01f816ebd1259388dca92fd1
Gabriel <gabrielantoniocp9@gmail.com>
2018-05-31 19:11:07 -0300

    Criando painel administrativo para o Admin

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('painel', require('./components/Painel.vue'));
 Vue.component('legiaomenu', require('./components/Menu.vue'));
 
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<example-component></example-component>
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-10">
+              <painel titulo="Dashboard">
+                <painel titulo="Usuários">
+
+                </painel>
+              </painel>
+          </div>
+      </div>
+  </div>
 @endsection
